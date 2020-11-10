615ddff604295fe767beaa013c26676812ef32f4
Sviatoslav Hamar <s.hamar@abtollc.com>
2020-11-10 02:04:28 +0200

    Upgrade interface

---
+++ Src/ToDoList/ToDoList/Views/SignUpWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ToDoList.Views
+{
+    /// <summary>
+    /// Interaction logic for SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+        public SignUpWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
