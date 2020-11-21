171c3b8266d3126763849d3ebb0e06e828339bc9
Max Lytvynchuk <lytvynchuk.official@gmail.com>
2020-11-21 13:03:38 +0200

    Created binding fields

--- Src/ToDoList/ToDoList/ViewModels/SignUpViewModel.cs
+++ Src/ToDoList/ToDoList/ViewModels/SignUpViewModel.cs
@@ -5,14 +5,42 @@ namespace ToDoList.ViewModels
 {
     class SignUpViewModel : INotifyPropertyChanged
     {
-        private UserService userLogic;
+        private UserService userService;
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private string userName;
+        public string UserName
+        {
+            get { return this.userName; }
+            set
+            {
+                // Implement with property changed handling for INotifyPropertyChanged
+                if (!string.Equals(this.userName, value))
+                {
+                    this.userName = value;
+                }
+            }
+        }
+
+        private string password1;
+
+        public string Password1
+        {
+            get { return password1; }
+            set
+            {
+                if(!string.Equals(this.password1, value))
+                {
+                    this.password1 = value;
+                }
+            }
+        }
 
         private void RegisterUser()
         {
             try
             {
-                userLogic.CreateUser();
+                userService.CreateUser(UserName, Password1, password2)
             }catch (Exception e)
             {
                 e.Message == "Passoword Error"
