89ddc3ede52312193c18cd4061d50a29bba43121
Dasha <daria.penkovaa@gmail.com>
2020-11-23 10:11:42 +0200

    Implemented TaskService

---
+++ Src/ToDoList/ToDoList.Logic/Interfaces/ITaskService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Database.Entities;
+using Task = System.Threading.Tasks.Task;
+namespace ToDoList.Logic.Interfaces
+{
+    interface ITaskService
+    {
+        Task CreateTaskAsync(Database.Entities.Task task);
+        Task EditTaskAsync(Database.Entities.Task task);
+        Task DeleteTaskAsync(Database.Entities.Task task);
+    }
+}
+
+
